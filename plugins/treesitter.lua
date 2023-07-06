93c5b86b52cf6aaece0bd088ca2d889365a422a3
icheos <13967864790a@gmail.com>
2023-07-06 23:51:10 +0800

    backup01

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "c", "java", "python", "cpp", "markdown" },
+    ensure_installed = { "lua", "c", "java", "python", "cpp", "markdown", "gdscript" },
   },
 }
